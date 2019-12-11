89deb454cf2f7ee7a8f84fcb985dbba867e50efd
PAVEL TKACHEV <phoenixweiss@ya.ru>
2019-12-11 22:28:30 +0300

    upd

---
+++ lesson_2.js
@@ -0,0 +1,46 @@
+// Типы данных для разных переменных
+
+// Целые числа (integer), в js это тип number
+var my_integer = 1;
+
+// Числа с плавающей точкой (float), в js это также тип number
+var my_float = 1.01;
+
+// Строки (string)
+var my_string = '"Моя строка"';
+
+// Особый тип переменной которая не задана (undefined)
+var my_undef;
+
+// «Булев» тип (boolean), обозначающий логические ИСТИНУ или ЛОЖЬ (true, false)
+var my_bool = true;
+
+// Простые массивы (array), в js это тип object
+var my_array = [my_integer, 'azaza', 2.99, true];
+
+// Именованные массивы типа ключ-значение (key-value array), в js это также тип object
+var my_obj = {name:'Vasya', age:21};
+
+// Функция, отображающая значение переменной и её тип
+function show_me_type(myvar) {
+  console.log('Переменная ' + myvar + ' типа ' + typeof(myvar));
+};
+
+// Для каждой из перечисленных выше переменных
+show_me_type(my_integer);
+show_me_type(my_float);
+show_me_type(my_string);
+show_me_type(my_undef);
+show_me_type(my_bool);
+show_me_type(my_array);
+show_me_type(my_obj);
+
+// Записываем данные в js-объект, присваивая значение по ключу
+
+var my_surname = "Pupkin"
+
+var my_fullname = my_obj.name + ' ' + my_surname
+
+my_obj.name = my_fullname
+
+console.log(my_obj);
---
+++ lesson_3.js
@@ -0,0 +1,26 @@
+function compare_it(a,b) {
+
+  // выводим в консоль то что мы складываем
+  console.log("Результат сравнения " + a + " и " + b);
+
+  // выводим в консоль результат сравнения
+
+  if ( a < b ) {
+    console.log('Полчается, что ' + a + ' меньше ' + b);
+  } else if ( a > b ) {
+    console.log('Полчается, что ' + a + ' больше ' + b);
+  } else {
+    console.log('Полчается, что ' + a + ' и ' + b + ' равны');
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    array[i]
+  }
+
+}
+
+compare_it(2,4);
+
+compare_it(9,7);
+
+compare_it(1,1);
