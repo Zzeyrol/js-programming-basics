85f2baf1b7adec9bc28275793e9a78d967385f5b
PAVEL TKACHEV <phoenixweiss@ya.ru>
2019-12-20 13:01:07 +0300

    lesson5 update

--- lesson_5.js
+++ lesson_5.js
@@ -1,4 +1,21 @@
+var elem;
+var output;
+
 function showMeAlert() {
   elem = document.getElementsByTagName("input")[0].value;
   alert( "Текст из поля: " + elem );
+}
+
+function writeToConsole() {
+  elem = document.getElementById("myinput").value;
+  console.log( "Текст из поля: " + elem );
+}
+
+function writeToHeader() {
+  elem = document.getElementsByName("mytext")[0].value;
+  output = document.getElementsByClassName("myoutput")[0];
+
+  console.log(output);
+
+  output.innerHTML = elem;
 }
