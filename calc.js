7e75f271ab50b929caf1ae0d6ddd093d24bc7bd5
Zer <alex_good2510@mail.ru>
2019-12-22 19:25:19 +0300

    Сделал вычитание

--- calc.js
+++ calc.js
@@ -6,5 +6,11 @@ var just = Number(document.getElementById('second_number').value)
 var otvet = go + just
 document.getElementById("result").innerHTML = otvet 
 }
+function vijitanie(){
+var go = Number(document.getElementById('first_number').value)
+var just = Number(document.getElementById('second_number').value)	
+var otvet = go - just
+document.getElementById("result").innerHTML = otvet 
+}
 
 console.log('Скрипт загружен');
