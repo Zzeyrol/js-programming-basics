92904d3d4426a918f83dfafe3fe2cb89274da062
Zer <alex_good2510@mail.ru>
2019-12-22 12:35:03 +0300

    Update calc.js
    Работает

--- calc.js
+++ calc.js
@@ -1,3 +1,5 @@
 // Здесь должен быть итоговый код калькулятора
+go = document.getElementById('first_number').value
+just = document.getElementById('second_number').value
 
 console.log('Скрипт загружен');
