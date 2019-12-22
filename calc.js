5196848c9b51f48d13205868264dc0096b24cebd
Zer <alex_good2510@mail.ru>
2019-12-22 19:27:27 +0300

    Все готово

--- calc.js
+++ calc.js
@@ -12,5 +12,16 @@ var just = Number(document.getElementById('second_number').value)
 var otvet = go - just
 document.getElementById("result").innerHTML = otvet 
 }
-
+function umnojnie(){
+var go = Number(document.getElementById('first_number').value)
+var just = Number(document.getElementById('second_number').value)	
+var otvet = go * just
+document.getElementById("result").innerHTML = otvet 
+}
+function delenie(){
+var go = Number(document.getElementById('first_number').value)
+var just = Number(document.getElementById('second_number').value)	
+var otvet = go / just
+document.getElementById("result").innerHTML = otvet 
+}
 console.log('Скрипт загружен');
