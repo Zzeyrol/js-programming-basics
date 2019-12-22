6b27143bb2ed73b64d805997a4ede770dc1978c6
Zer <alex_good2510@mail.ru>
2019-12-22 19:02:01 +0300

    складывает строки а не числа

--- calc.js
+++ calc.js
@@ -1,5 +1,9 @@
 // Здесь должен быть итоговый код калькулятора
+
+function summ(){
 go = document.getElementById('first_number').value
 just = document.getElementById('second_number').value
+console.log( go + just)
+}
 
 console.log('Скрипт загружен');
